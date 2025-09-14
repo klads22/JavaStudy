c8c2778c089a5978436d48f083d63c520dcd711c
LSJ <LSJ@DESKTOP-34BUVCD>
2025-09-14 20:02:05 +0900

    java

--- src/ch02/sec02/InteherLiteralExample.java
+++ src/ch02/sec02/InteherLiteralExample.java
@@ -3,10 +3,10 @@ package ch02.sec02;
 public class InteherLiteralExample {
 	
 	public static void main(String[] args) {
-		int var1 = 0b1011;	//2진수
-		int var2 = 0206;	//8진수
-		int var3 = 365;		//10진수
-		int var4 = 0xB3;	//16진수
+		int var1 = 0b1011;	// 2진수
+		int var2 = 0206;	// 8진수
+		int var3 = 365;		// 10진수
+		int var4 = 0xB3;	// 16진수
 		
 		System.out.println("var1:" + var1);
 		System.out.println("var2:" + var2);
