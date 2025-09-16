aa4236850718abb3f3948912f902d7d103cfcd69
LSJ <LSJ@DESKTOP-34BUVCD>
2025-09-16 23:06:00 +0900

    0916

--- src/ch02/sec02/InteherLiteralExample.java
+++ src/ch02/sec02/InteherLiteralExample.java
@@ -3,7 +3,7 @@ package ch02.sec02;
 public class InteherLiteralExample {
 	
 	public static void main(String[] args) {
-		int var1 = 0b1011;	// 2진수
+		int var1 = 0b1011;	//2진수
 		int var2 = 0206;	// 8진수
 		int var3 = 365;		// 10진수
 		int var4 = 0xB3;	// 16진수
