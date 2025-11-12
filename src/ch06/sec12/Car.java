13499ae9ef67deb8afc4434d1b2e3c2129311956
LSJ <leesj03a@naver.com>
2025-11-12 23:27:47 +0900

    1112

---
+++ src/ch06/sec12/Car.java
@@ -0,0 +1,12 @@
+package ch06.sec12;
+
+import ch06.sec12.hankook.SnowTire;
+import ch06.sec12.kumho.AllSeasonTire;
+
+
+public class Car {
+	ch06.sec12.hankook.Tire tire1 = new ch06.sec12.hankook.Tire();
+	ch06.sec12.kumho.Tire tire2 = new ch06.sec12.kumho.Tire();
+	ch06.sec12.hankook.SnowTire tire3 = new ch06.sec12.hankook.SnowTire();
+	ch06.sec12.kumho.AllSeasonTire tire4 = new ch06.sec12.kumho.AllSeasonTire();	
+}
